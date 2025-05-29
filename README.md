ec20f4711bca7db419faf1bcf96d80be8e298ea4
somnionocte <somnionocte@proton.me>
2025-05-29 21:56:46 +0300

    UPDATE

--- README.md
+++ README.md
@@ -9,3 +9,66 @@ Yes, and before you say that, just show you my only argument in favor of this na
 Here's an example from my application (in development): 
 
 https://github.com/user-attachments/assets/15a283fa-5ef2-4403-b01c-f1094029cc23
+
+### How to use
+
+Apparently you need to enable predictive gesture support in the manifest:
+
+```xml
+<application
+    android:enableOnBackInvokedCallback="true">
+    ...
+</application>
+```
+
+Define some screens:
+
+```kotlin
+object Screens {
+    data class User(val id: String) : Screen
+    
+    object Settings : Screen {
+        object General : Screen
+        object Theme : Screen
+    }
+}
+```
+
+And use a router that will transmit a screen with the type defined earlier.
+`NullScreen` is the initial screen, which obviously has no arguments and after which the user's home screen logically follows.
+
+```kotlin
+setContent {
+    MyAppTheme {
+        ScreenRouter { screen ->
+            when (screen) {
+                is NullScreen -> MainScreen()
+                is Screens.User -> UserScreen(screen.id)
+                is Screens.Settings -> SettingsScreen()
+                is Screens.Settings.General -> GeneralSettingsScreen()
+                is Screens.Settings.Theme -> ThemeSettingsScreen()
+            }
+        }
+    }
+}
+```
+
+### Install
+
+Add JitPack repository in your settings.gradle.kts:
+
+```kotlin
+	dependencyResolutionManagement {
+		repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+		repositories {
+			mavenCentral()
+            maven { url = uri("https://jitpack.io") }
+		}
+	}
+```
+
+Add the dependency:
+
+```kotlin
+implementation("com.github.SomnioNocte:screen-router:0.8.0")
+```
--- app/src/main/java/com/somnionocte/screen_router/MainActivity.kt
+++ app/src/main/java/com/somnionocte/screen_router/MainActivity.kt
@@ -4,28 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.FilledTonalIconButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.somnionocte.screen_router.ui.theme.Screen_routerTheme
+import androidx.compose.ui.unit.sp
+import com.somnionocte.screen_router.ui.theme.MyAppTheme
 
 object Screens {
-    object Screen1 : Screen
-    object Screen2 : Screen
-    object Screen3 : Screen
+    data class User(val id: String) : Screen
+    object Settings : Screen {
+        object General : Screen
+        object Theme : Screen
+    }
 }
 
 class MainActivity : ComponentActivity() {
@@ -34,99 +35,130 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            Screen_routerTheme {
+            MyAppTheme {
                 ScreenRouter { screen ->
                     when (screen) {
-                        is NullScreen -> Screent()
-                        is Screens.Screen1 -> ScreenQ()
-                        is Screens.Screen2 -> ScreenW()
-                        is Screens.Screen3 -> ScreenE()
+                        is NullScreen -> MainScreen()
+                        is Screens.User -> UserScreen(screen.id)
+                        is Screens.Settings -> SettingsScreen()
+                        is Screens.Settings.General -> GeneralSettingsScreen()
+                        is Screens.Settings.Theme -> ThemeSettingsScreen()
                     }
                 }
             }
         }
     }
+}
 
-    @Composable
-    fun Screent() {
-        val router = LocalScreenRouter.current
+@Composable
+fun UserScreen(id: String) {
+    val router = LocalScreenRouter.current
 
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Column(
-                Modifier.fillMaxSize().padding(innerPadding),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+    Scaffold { padding ->
+        Column(
+            Modifier.padding(padding).padding(24.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Text("Profile", fontSize = 28.sp)
+
+            Text("id: $id", fontSize = 22.sp)
+
+            FilledTonalButton(
+                onClick = { router.pop() }
             ) {
-                Text("Main screen")
-
-                Spacer(Modifier.height(16.dp))
-
-                FilledTonalButton(
-                    onClick = { router.go(Screens.Screen1) }
-                ) {
-                    Text("Go to screen 1")
-                }
+                Text("Go back")
             }
         }
     }
+}
 
-    @Composable
-    fun ScreenQ() {
-        val router = LocalScreenRouter.current
+@Composable
+fun MainScreen() {
+    val router = LocalScreenRouter.current
 
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Column(
-                Modifier.fillMaxSize().padding(innerPadding),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+    Scaffold { padding ->
+        Column(
+            Modifier.padding(padding).padding(24.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Text("Main screen", fontSize = 28.sp)
+
+            FilledTonalIconButton(
+                onClick = { router.go(Screens.Settings) }
             ) {
-                Text("First screen")
+                Icon(Icons.Rounded.Settings, "Settings")
+            }
 
-                Spacer(Modifier.height(16.dp))
-
-                FilledTonalButton(
-                    onClick = { router.go(Screens.Screen2) }
-                ) {
-                    Text("Go to screen 2")
-                }
+            FilledTonalButton(
+                onClick = { router.go(Screens.User(id = "123")) }
+            ) {
+                Text("Go to some profile")
             }
         }
     }
+}
 
-    @Composable
-    fun ScreenW() {
-        val router = LocalScreenRouter.current
+@Composable
+fun SettingsScreen() {
+    val router = LocalScreenRouter.current
 
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Column(
-                Modifier.fillMaxSize().padding(innerPadding),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+    Scaffold { padding ->
+        Column(
+            Modifier.padding(padding).padding(24.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Text("Settings", fontSize = 28.sp)
+
+            FilledTonalButton(
+                onClick = { router.go(Screens.Settings.General) }
             ) {
-                Text("Second screen")
+                Text("General settings")
+            }
 
-                Spacer(Modifier.height(16.dp))
-
-                FilledTonalButton(
-                    onClick = { router.go(Screens.Screen3) }
-                ) {
-                    Text("Go to screen 3")
-                }
+            FilledTonalButton(
+                onClick = { router.go(Screens.Settings.Theme) }
+            ) {
+                Text("Theme settings")
             }
         }
     }
+}
 
-    @Composable
-    fun ScreenE() {
-        val router = LocalScreenRouter.current
+@Composable
+fun ThemeSettingsScreen() {
+    val router = LocalScreenRouter.current
 
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Column(
-                Modifier.fillMaxSize().padding(innerPadding),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+    Scaffold { padding ->
+        Column(
+            Modifier.padding(padding).padding(24.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Text("Theme settings", fontSize = 28.sp)
+
+            FilledTonalButton(
+                onClick = { router.pop() }
             ) {
-                Text("Last screen")
+                Text("Go back")
+            }
+        }
+    }
+}
+
+@Composable
+fun GeneralSettingsScreen() {
+    val router = LocalScreenRouter.current
+
+    Scaffold { padding ->
+        Column(
+            Modifier.padding(padding).padding(24.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Text("General settings", fontSize = 28.sp)
+
+            FilledTonalButton(
+                onClick = { router.pop() }
+            ) {
+                Text("Go back")
             }
         }
     }
